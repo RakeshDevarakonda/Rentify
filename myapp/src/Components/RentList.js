3962a246d270d93c6dff667fc6433a4fdd50bc2e
Rakesh <rakeshdevarakonda2000@gmail.com>
2024-05-28 20:52:45 +0530

    added backend images

--- myapp/src/Components/RentList.js
+++ myapp/src/Components/RentList.js
@@ -450,7 +450,7 @@ export default function RentList({ editmode }) {
                                 to={`singleproperty/${property._id}`}
                               >
                                 <img
-                                  src={`${process.env.REACT_APP_BACKENDSERVERNAME}/${property.photos[0]}`}
+                                  src={`${process.env.REACT_APP_BACKENDSERVERNAME}/rentimages/${property.photos[0]}`}
                                   alt=""
                                 />
                               </Link>
@@ -460,7 +460,7 @@ export default function RentList({ editmode }) {
                                 to={`/singleproperty/${property._id}`}
                               >
                                 <img
-                                  src={`${process.env.REACT_APP_BACKENDSERVERNAME}/${property.photos[0]}`}
+                                  src={`${process.env.REACT_APP_BACKENDSERVERNAME}/rentimages/${property.photos[0]}`}
                                   alt=""
                                 />
                               </Link>
