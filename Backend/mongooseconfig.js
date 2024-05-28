246f981d0b61cc5473f2d022573df76ba1b510e5
Rakesh <rakeshdevarakonda2000@gmail.com>
2024-05-28 20:11:01 +0530

    first commit

--- Backend/mongooseconfig.js
+++ Backend/mongooseconfig.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 export const mongoosedatabse = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to database');
+
+    console.log("process.env.MONGODB_URI",process.env.MONGODB_URI);
   } catch (error) {
     console.error(error);
   }
